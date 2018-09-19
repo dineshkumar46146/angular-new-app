6916b3fc2a5b73b346de0aedaecbda0261ef24f4
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-09-19 23:55:37 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
